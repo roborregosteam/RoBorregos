89f328c0ae40b014d748437f5b1552e8fefb3017
Oscar-gg <oscar.arreola.jr@gmail.com>
2023-11-07 20:55:20 -0600

    Navbar implementation

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  NavbarMenuToggle,
+  NavbarMenuItem,
+  NavbarMenu,
+} from "@nextui-org/react";
+import { RoBorregosLogo } from "./RoBorregosLogo.jsx";
+
+export default function NavbarLayout() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
+  const links = [
+    { name: "Home", href: "/", className: "text-white", active: true },
+    { name: "Members", href: "/", className: "text-white" },
+    { name: "About", href: "/", className: "text-white" },
+    { name: "Projects", href: "/", className: "text-white" },
+    { name: "Candidates", href: "/", className: "text-white" },
+    { name: "Gallery", href: "/", className: "text-white" },
+  ];
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen} className="bg-black">
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden text-white"
+        />
+        <NavbarBrand>
+          {/* <AcmeLogo /> */}
+          <RoBorregosLogo />
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {links.map((item, index) => (
+          <NavbarItem
+            key={`${item.name}-${index}`}
+            {...(item.active ? { isActive: true } : { isActive: false })}
+          >
+            <Link
+              color={item.color}
+              className={item.className}
+              href={item.href}
+              size="sm"
+            >
+              {item.name}
+            </Link>
+          </NavbarItem>
+        ))}
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Customers
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Support us
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {links.map((item, index) => (
+          <NavbarMenuItem key={`${item.name}-${index}`}>
+            <Link
+              color={
+                index === 2
+                  ? "primary"
+                  : index === links.length - 1
+                  ? "danger"
+                  : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
---
+++ src/components/navbar/RoBorregosLogo.jsx
@@ -0,0 +1,4 @@
+
+export const RoBorregosLogo = () => (
+  <img src="https://jwxqxlsoznvanzoqgpvb.supabase.co/storage/v1/object/public/profile-pictures/Perfil_default.png" height={36} width={36}/>
+);
