458fb7a0f2492d668b5dece4187843301b195033
Alejandra Coeto <a01285221@tec.mx>
2023-11-28 22:34:40 -0600

    animations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,19 @@ const { nextui } = require("@nextui-org/react");
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-	"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				pulse: {
+					'0%, 50%': { opacity: 0 },
+					'100%': { opacity: 0.9 },
+				},
+			},
+			animation: {
+				pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1)',
+			},
+		},
 	},
 	darkMode: "class",
 	plugins: [nextui()],
