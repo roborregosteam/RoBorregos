ec8ed7e4bda1b7bcf633d6932c56ed0e8f80373e
Jonatandlr <A01735993@tec.mx>
2024-05-08 19:35:44 -0600

    chore: Add Home component and update layoutTest.astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,14 +5,26 @@ export default {
 		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
+			backgroundImage: {
+				'Homebg': 'linear-gradient(180deg, rgba(0,0,0,0.55), rgba(0,0,0,0.55)),url("/images/HomeBg.jpg")',
+				'Homebg-degradado': 'linear-gradient(180deg, rgba(0,0,0,0), rgba(0,0,0,0.0),rgba(0,0,0,0),rgba(0,0,0,0),rgba(0,0,0,0),rgba(0,0,0,0.75))',
+			  },
 			keyframes: {
 				pulse: {
 					'0%, 50%': { opacity: 0 },
 					'100%': { opacity: 1 },
 				},
+				blinkfast: {
+					'0%, 50%, 100%': { opacity: 1 },
+					'25%, 75%': { opacity: 0 },
+				}
+				
+
 			},
 			animation: {
 				pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1)',
+				blinkfast: 'blinkfast 0.8s linear infinite',
+				
 			},
 			fontFamily: {
 				'tomorrow': ['Tomorrow', 'sans-serif'],
