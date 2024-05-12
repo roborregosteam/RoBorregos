2fc10938451ad97b97f251ffab453547cebd95fb
Jonatandlr <A01735993@tec.mx>
2024-05-12 07:20:37 -0600

    fix gradient home

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import { fade } from "astro/dist/transitions";
+
 const { nextui } = require("@nextui-org/react");
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -7,7 +9,7 @@ export default {
 		extend: {
 			backgroundImage: {
 				'Homebg': 'linear-gradient(180deg, rgba(0,0,0,0.55), rgba(0,0,0,0.55)),url("/images/HomeBg.jpg")',
-				'Homebgdegra': 'linear-gradient(180deg, rgba(0,0,0,0), rgba(0,0,0,0),rgba(0,0,0,0),rgba(0,0,0,0.55),rgba(0,0,0,0.95))',
+				'Homebg_gradient': 'linear-gradient(180deg, rgba(0,0,0,0), rgba(0,0,0,0),rgba(0,0,0,0),rgba(0,0,0,0.55),rgba(0,0,0,0.95))',
 			  },
 			keyframes: {
 				pulse: {
@@ -17,13 +19,16 @@ export default {
 				blinkfast: {
 					'0%, 50%, 100%': { opacity: 1 },
 					'25%, 75%': { opacity: 0 },
-				}
-				
-
+				},
+				fadeIn: {
+					'0%': { opacity: 0 },
+					'100%': { opacity: 1 },
+				},
 			},
 			animation: {
 				pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1)',
 				blinkfast: 'blinkfast 0.8s linear infinite',
+				fadeIn: 'fadeIn 2s ease-in-out forwards',
 				
 			},
 			fontFamily: {
